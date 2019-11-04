f351b7801fe3abff13e0eeb8aa31ba057ca3b741
Diego Gama <ti.diegogama@gmail.com>
2019-11-03 22:56:31 -0300

    Adicionar chamado

---
+++ amazon-frete/src/main/java/br/com/fiap/frete/amazonfrete/contorller/FreteController.java
@@ -0,0 +1,22 @@
+package br.com.fiap.frete.amazonfrete.contorller;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.frete.amazonfrete.model.Frete;
+
+@RestController
+@RequestMapping("/frete")
+public class FreteController {
+
+	@PostMapping
+	public Double calcular(@Valid @RequestBody Frete frete) {
+		Double valor = 5.0;
+		return valor;
+	}
+	
+}
---
+++ amazon-frete/src/main/java/br/com/fiap/frete/amazonfrete/model/Frete.java
@@ -0,0 +1,25 @@
+package br.com.fiap.frete.amazonfrete.model;
+
+public class Frete {
+
+	private String cep;
+	
+	private String peso;
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getPeso() {
+		return peso;
+	}
+
+	public void setPeso(String peso) {
+		this.peso = peso;
+	}
+
+}
